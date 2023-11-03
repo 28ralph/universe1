0b19fb111c8edacd14cf22c136286d3dba27b97a
28ralph <lee.ralph@sbcglobal.net>
2023-11-03 09:09:57 -0500

    universe 4

--- universe.js
+++ universe.js
@@ -6,7 +6,7 @@ class Human {
         this.accuracy = accuracy;
     }
 
-    attack(){
+    attack() {
         console.log("You are attacking an alien!");
     }
 }
@@ -18,37 +18,37 @@ class Alien {
         this.accuracy = accuracy;
     }
 
-    attack(){
+    attack() {
         console.log("Alien attacking human");
     }
 }
 
 let human = new Human(20, 5, .7);
 
-let alien = [2];
+let alien = [6];
 let x, y, z;
-for (let i = 0; i<2; i++){
-    x = Math.random() * (3) + 3
-    y = Math.random() * (2) + 2
+for (let i = 0; i < 6; i++) {
+    x = Math.floor(Math.random() * (4) + 6);
+    y = Math.floor(Math.random() * (3) + 2)
     z = Math.random() * (.2) + .6
-    alien[i] = new Alien(x,y,z)
+    alien[i] = new Alien(x, y, z)
 
-   
+
 }
 
 let i = 0;
 
-while (i <= 1){
+while (i < 6) {
     human.attack()
-    
-    if(Math.random() < human.accuracy){
+
+    if (Math.random() < human.accuracy) {
 
         console.log("You hit the alien!!!");
         console.log("You have done 5 damage");
         alien[i].hull -= human.firepower;
         console.log("Alien has " + alien[i].hull + " hull remaining.");
-        if (alien[i].hull <= 0){
-           
+        if (alien[i].hull <= 0) {
+
             console.log("Human wins round " + (i + 1));
             ++i;
         }
@@ -59,4 +59,35 @@ while (i <= 1){
         console.log("You missed");
         // continue
     }
+
+    // alien[i].attack()
+    console.log("Alien attacks");
+    if (Math.random() < alien[i].accuracy) {
+
+        console.log("Alien hit the human!!!");
+        console.log("Alien have done" + alien[i].firepower + " damage");
+        human.hull -= alien[i].firepower;
+        console.log("Human has " + human.hull + " hull remaining.");
+        if (human.hull <= 0) {
+
+            console.log("Alien wins " + (i + 1));
+            console.log("Game Over!!");
+            break;
+
+        }
+
+        // break;
+    }
+    else {
+        console.log("Alien missed");
+        // continue
+    }
+
+
+
+
+
+
+
+
 }
